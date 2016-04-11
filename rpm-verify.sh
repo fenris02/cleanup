904c164cd7477fa9848e227cc923fb785f690a9b
d. johnson <fenris02@fedoraproject.org>
2016-04-10 22:27:36 -0500

    shellcheck / quotes

--- rpm-verify.sh
+++ rpm-verify.sh
@@ -10,7 +10,7 @@ if [ "$(/usr/bin/whoami)" != "root" ]; then
 fi
 
 DS=$(/bin/date +%Y%m%d)
-TMPDIR=$(/bin/mktemp -d ${TMPDIR:-/tmp}/${0##*/}-XXXXX.log)
+TMPDIR=$(/bin/mktemp -d "${TMPDIR:-/tmp}/${0##*/}-XXXXX.log")
 [ -d "${TMPDIR}" ] || mkdir -p "${TMPDIR}"
 
 if [ -f /etc/sysconfig/prelink ]; then
@@ -25,21 +25,21 @@ fi
 /bin/rm /var/lib/rpm/__db.00?
 
 echo "rpm-Va: This may take 12mins or longer, please wait ... (Might be a good time for coffee)"
-time /bin/rpm -Va > ${TMPDIR}/rpm-va2_${DS}.txt 2>&1
+time /bin/rpm -Va > "${TMPDIR}/rpm-va2_${DS}.txt" 2>&1
 # Filter out prelink messages, kmod files, and kernel-devel files:
 /bin/egrep -v '^(prelink: /|S\..\.\.\.\.\.\.    /|.{9}    /lib/modules/.*/modules\.|.{9}    /usr/src/kernels/)' \
-  ${TMPDIR}/rpm-va2_${DS}.txt > ${TMPDIR}/RPM-VA2_${DS}.txt
+  "${TMPDIR}/rpm-va2_${DS}.txt" > "${TMPDIR}/RPM-VA2_${DS}.txt"
 
 echo "Generating reports ..."
-/bin/egrep -v '^.{9}  c /' ${TMPDIR}/RPM-VA2_${DS}.txt > ${TMPDIR}/URGENT-REVIEW_${DS}.txt
-/bin/egrep '^.{9}  c /' ${TMPDIR}/RPM-VA2_${DS}.txt > ${TMPDIR}/REVIEW-CONFIGS_${DS}.txt
-/bin/find /etc -name '*.rpm?*' > ${TMPDIR}/REVIEW-OBSOLETE-CONFIGS_${DS}.txt
+/bin/egrep -v '^.{9}  c /' "${TMPDIR}/RPM-VA2_${DS}.txt" > "${TMPDIR}/URGENT-REVIEW_${DS}.txt"
+/bin/egrep '^.{9}  c /' "${TMPDIR}/RPM-VA2_${DS}.txt" > "${TMPDIR}/REVIEW-CONFIGS_${DS}.txt"
+/bin/find /etc -name '*.rpm?*' > "${TMPDIR}/REVIEW-OBSOLETE-CONFIGS_${DS}.txt"
 
 if [ \! -f /var/lib/yum/groups/installed ]; then
   # "group mark convert" converts the automatic data you get without using groups
   # as objects into groups as objects data. This makes it much easier to convert
   # to groups as objects without having to reinstall.
-  if [ 0$(rpm -qf /etc/redhat-release --qf '%{version}\n') -gt 18 ]; then
+  if [ "0$(rpm -qf /etc/redhat-release --qf '%{version}\n')" -gt 18 ]; then
     echo "F19 hack to help from upgrades"
     /usr/bin/yum group mark convert
   fi
@@ -56,63 +56,63 @@ echo "Requesting extra reporting tools to be installed ..."
 
 if [ -x /usr/sbin/semanage ]; then
   echo "Reporting SELinux policy ..."
-  TMPF=$(/bin/mktemp -u /tmp/${0##*/}-XXXXX.txt)
-  /usr/sbin/semanage -o $TMPF
-  /bin/mv $TMPF ${TMPDIR}/SELINUX-CUSTOM-CONFIG_${DS}.txt
+  TMPF="$(/bin/mktemp -u /tmp/${0##*/}-XXXXX.txt)"
+  /usr/sbin/semanage -o "$TMPF"
+  /bin/mv "$TMPF" "${TMPDIR}/SELINUX-CUSTOM-CONFIG_${DS}.txt"
 fi
 
 if [ -x /usr/bin/rpmdev-rmdevelrpms ]; then
   echo "Reporting devel packages"
-  /usr/bin/rpmdev-rmdevelrpms -l > ${TMPDIR}/SHOW-DEVELRPMS_${DS}.txt
+  /usr/bin/rpmdev-rmdevelrpms -l > "${TMPDIR}/SHOW-DEVELRPMS_${DS}.txt"
 fi
 
 echo "Finding installed packages ..."
 if [ -x /usr/bin/show-installed ]; then
-  /usr/bin/show-installed -f kickstart -e -o ${TMPDIR}/SHOW-INSTALLED2_${DS}.txt
+  /usr/bin/show-installed -f kickstart -e -o "${TMPDIR}/SHOW-INSTALLED2_${DS}.txt"
 else
-  $(dirname $0)/show-installed -f kickstart -e -o ${TMPDIR}/SHOW-INSTALLED2_${DS}.txt
+  "$(dirname $0)/show-installed" -f kickstart -e -o "${TMPDIR}/SHOW-INSTALLED2_${DS}.txt"
 fi
-/bin/sort -o ${TMPDIR}/SHOW-INSTALLED2_${DS}.txt ${TMPDIR}/SHOW-INSTALLED2_${DS}.txt
+/bin/sort -o "${TMPDIR}/SHOW-INSTALLED2_${DS}.txt" "${TMPDIR}/SHOW-INSTALLED2_${DS}.txt"
 
 if [ -x /usr/sbin/yumdb ]; then
   echo "Locating rpm packages that were installed without yum ..."
-  /usr/sbin/yumdb --noplugins unset from_repo > ${TMPDIR}/SHOW-EXTERNAL_${DS}.txt
+  /usr/sbin/yumdb --noplugins unset from_repo > "${TMPDIR}/SHOW-EXTERNAL_${DS}.txt"
 fi
 
 echo "Reporting Problem RPMs"
 if [ -x /usr/bin/package-cleanup ]; then
-  /usr/bin/package-cleanup -q --problems > ${TMPDIR}/PROBLEM-PACKAGES_${DS}.txt
+  /usr/bin/package-cleanup -q --problems > "${TMPDIR}/PROBLEM-PACKAGES_${DS}.txt"
 else
-  /bin/rpm -Va --nofiles --noscripts > ${TMPDIR}/PROBLEM-PACKAGES_${DS}.txt
+  /bin/rpm -Va --nofiles --noscripts > "${TMPDIR}/PROBLEM-PACKAGES_${DS}.txt"
 fi
 
 if [ -x /usr/bin/package-cleanup ]; then
   echo "Reporting Duplicate RPMs"
-  /usr/bin/package-cleanup -q --dupes > ${TMPDIR}/DUPLICATE-PACKAGES_${DS}.txt
+  /usr/bin/package-cleanup -q --dupes > "${TMPDIR}/DUPLICATE-PACKAGES_${DS}.txt"
 fi
 
 if [ -x /usr/bin/package-cleanup ]; then
   echo "Reporting Orphan RPMs"
-  /usr/bin/package-cleanup -q --orphans > ${TMPDIR}/ORPHANED-PACKAGES_${DS}.txt
+  /usr/bin/package-cleanup -q --orphans > "${TMPDIR}/ORPHANED-PACKAGES_${DS}.txt"
 fi
 
 if [ -x /usr/bin/repoquery ]; then
   echo "Collect leaf RPMs"
   /usr/bin/repoquery --installed --qf "%{nvra} - %{yumdb_info.reason}" \
-    `/usr/bin/package-cleanup --leaves -q --all` \
+    $(/usr/bin/package-cleanup --leaves -q --all) \
     |/bin/grep '\- dep' \
-    |while read n a a; do \
-      echo remove $n
-    done > ${TMPDIR}/SHOW-LEAVES_${DS}.txt
+    |while read -r n a a; do \
+      echo "remove $n"
+    done > "${TMPDIR}/SHOW-LEAVES_${DS}.txt"
 fi
 
 echo "Collect list of enabled repos"
-/usr/bin/yum repolist > ${TMPDIR}/YUM-REPOLIST_${DS}.txt
+/usr/bin/yum repolist > "${TMPDIR}/YUM-REPOLIST_${DS}.txt"
 
 echo "Collecting distribution-synchronization differences"
 echo n |\
   /usr/bin/yum distro-sync |\
-  /bin/sed -e '1,/Dependencies Resolved/d;' > ${TMPDIR}/YUM-DISTROSYNC_${DS}.txt
+  /bin/sed -e '1,/Dependencies Resolved/d;' > "${TMPDIR}/YUM-DISTROSYNC_${DS}.txt"
 
 /bin/cat - <<EOT
 ==========
@@ -126,25 +126,25 @@ TMPDIR = ${TMPDIR}
 EOT
 
 for fp in ${TMPDIR}/{YUM-REPOLIST,YUM-DISTROSYNC,URGENT-REVIEW,REVIEW-CONFIGS,PROBLEM-PACKAGES,DUPLICATE-PACKAGES,ORPHANED-PACKAGES,REVIEW-OBSOLETE-CONFIGS,SELINUX-CUSTOM-CONFIG,SHOW-DEVELRPMS,SHOW-EXTERNAL,SHOW-LEAVES,SHOW-INSTALLED2}*_${DS}.txt; do
-  if [ -s $fp ]; then
-    /bin/cat - >> ${TMPDIR}/fpaste-output_${DS}.txt <<EOT
+  if [ -s "$fp" ]; then
+    /bin/cat - >> "${TMPDIR}/fpaste-output_${DS}.txt" <<EOT
 ===============================================================================
 ===== $fp
 ===============================================================================
 EOT
     ### Limit each output file to 1000 lines to prevent excessive flooding.
-    WCL=$(/usr/bin/wc -l $fp |/usr/bin/gawk '{print$1}')
-    if [ $WCL -gt 1000 ]; then
+    WCL="$(/usr/bin/wc -l "$fp" |/usr/bin/gawk '{print$1}')"
+    if [ "$WCL" -gt 1000 ]; then
       echo "*** File $fp truncated to 1000 lines, was $WCL lines. ***"
-      echo "*** File $fp truncated to 1000 lines, was $WCL lines. ***" >> ${TMPDIR}/fpaste-output_${DS}.txt
+      echo "*** File $fp truncated to 1000 lines, was $WCL lines. ***" >> "${TMPDIR}/fpaste-output_${DS}.txt"
     fi
-    /usr/bin/head -n1000 $fp >> ${TMPDIR}/fpaste-output_${DS}.txt
+    /usr/bin/head -n1000 "$fp" >> "${TMPDIR}/fpaste-output_${DS}.txt"
   fi
 done
-echo fpaste ${TMPDIR}/fpaste-output_${DS}.txt
+echo "fpaste ${TMPDIR}/fpaste-output_${DS}.txt"
 
 if [ "x$1" != "xNOPOST" ]; then
-  /usr/bin/fpaste ${TMPDIR}/fpaste-output_${DS}.txt
+  /usr/bin/fpaste "${TMPDIR}/fpaste-output_${DS}.txt"
 fi
 
 #EOF
