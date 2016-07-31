f3d166862b07d1fde2ecfd0bdf8fed1ddbd305fc
d. johnson <fenris02@fedoraproject.org>
2016-07-30 22:53:39 -0500

    use a backup script lockfile, and local my.cnf for auth

--- mysql-backup2.sh
+++ mysql-backup2.sh
@@ -6,15 +6,24 @@
 # This will make the table-lock time very brief, allowing you to safely backup
 # large databases
 #
+# BUGS:
+# $TMPDIR remains behind after the script is run. (uncomment rm line near bottom to fix)
+#
+# Likely more effecient to run a slave db and backup from that instead.
+#
 
 # Setup environment
 DC="$(/bin/date +%Y%m%dT%H%M)"
 TMPDIR="$( /bin/mktemp -d "/var/tmp/${0##*/}.XXXXXXXXXX" )" || { echo "mktemp failed" >&2 ; exit 1 ; };
 SELF="${0##*/}"
+lockfile="$TMPDIR/$SELF.lockfile"
 
 # Configure options
 BK_DIR="$HOME/MySQL/mysql-backup-$DC"
-MYSQLPARAMS="-h localhost -u root -p secret-password"
+MYSQLUSER="root"
+MYSQLPASS="secret-password"
+MYSQLCNF="$TMPDIR/my.cnf"
+MYSQLPARAMS="-h localhost --my-config=$MYSQLCNF"
 
 [ -d "$BK_DIR" ] || mkdir -p "$BK_DIR"
 cd "$BK_DIR" || exit 1
@@ -26,53 +35,83 @@ LVM="${BLOCKDEV##*/}"
 VG="${LVM%-*}"
 LV="${LVM#*-}"
 
-# lock dbms
-/usr/bin/mysql "$MYSQLPARAMS" -e 'FLUSH TABLES WITH READ LOCK;'
+clean_house () {
+  /bin/rm -rf "TMPDIR"
+  exit $?
+}
 
-# Create snapshot
-/usr/sbin/lvcreate --extents 100%FREE --snapshot --name "snap-$DC" "/dev/$VG/$LV"
+# Ensure a clean exit
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null; then
+  trap clean_house INT TERM EXIT
 
-# unlock dbms
-/usr/bin/mysql "$MYSQLPARAMS" -e 'UNLOCK TABLES;'
+  # Create credentials file
+  cat - > "$MYSQLCNF" <<EOT
+[client]
+user=$MYSQLUSER
+password=$MYSQLPASS
+EOT
 
-# Mount snapshot
-mkdir -p "$TMPDIR/snap"
-/usr/bin/mount "/dev/$VG/snap-$DC" "$TMPDIR/snap"
+  # lock dbms
+  /usr/bin/mysql "$MYSQLPARAMS" -e 'FLUSH TABLES WITH READ LOCK;'
 
-# run dbms from snapshot
-echo 'select 1;' | /usr/bin/mysqld_safe 
-  --bootstrap \
-  --datadir="$TMPDIR/snap/$DATADIR" \
-  --pid-file="$TMPDIR/$SELF.pid" \
-  --skip-grant \
-  --skip-ndbcluster \
-  --skip-networking \
-  --skip-slave-start \
-  --socket="$TMPDIR/$SELF.sock" \
-  #
+  # Create snapshot
+  /usr/sbin/lvcreate --extents 100%FREE --snapshot --name "snap-$DC" "/dev/$VG/$LV"
 
-# dump from snapshot
-/usr/bin/mysqldump "$MYSQLPARAMS" \
-  --add-drop-database \
-  --all-databases \
-  --comments \
-  --create-db \
-  --create-info \
-  --dump-date \
-  --hex-blob \
-  --opt \
-  --order-by-primary \
-  --protocol=SOCKET \
-  --routines \
-  --socket="$TMPDIR/$SELF.sock" \
-  --triggers \
-  | /usr/bin/gzip > "$BK_DIR/mysql-backup-$DC.sql.gz"
+  # unlock dbms
+  /usr/bin/mysql "$MYSQLPARAMS" -e 'UNLOCK TABLES;'
 
-# stop dbms from snapshot
-/usr/bin/mysqladmin "$MYSQLPARAMS" --socket="$TMPDIR/$SELF.sock" -p shutdown
+  # Mount snapshot
+  mkdir -p "$TMPDIR/snap"
+  /usr/bin/mount "/dev/$VG/snap-$DC" "$TMPDIR/snap"
 
-# Remove snapshot
-/usr/bin/umount "$TMPDIR/snap"
-/usr/sbin/lvremove "/dev/$VG/snap-$DC"
+  # run dbms from snapshot
+  echo 'select 1;' | /usr/bin/mysqld_safe \
+    --bootstrap \
+    --datadir="$TMPDIR/snap/$DATADIR" \
+    --pid-file="$TMPDIR/$SELF.pid" \
+    --skip-grant \
+    --skip-ndbcluster \
+    --skip-networking \
+    --skip-slave-start \
+    --socket="$TMPDIR/$SELF.sock" \
+    #
+
+  # dump from snapshot
+  /usr/bin/mysqldump "$MYSQLPARAMS" \
+    --add-drop-database \
+    --all-databases \
+    --comments \
+    --create-db \
+    --create-info \
+    --dump-date \
+    --hex-blob \
+    --opt \
+    --order-by-primary \
+    --protocol=SOCKET \
+    --routines \
+    --socket="$TMPDIR/$SELF.sock" \
+    --triggers \
+    | /usr/bin/gzip > "$BK_DIR/mysql-backup-$DC.sql.gz"
+
+  # stop dbms from snapshot
+  /usr/bin/mysqladmin "$MYSQLPARAMS" --socket="$TMPDIR/$SELF.sock" -p shutdown
+
+  # Remove snapshot
+  /usr/bin/umount "$TMPDIR/snap"
+  /usr/sbin/lvremove "/dev/$VG/snap-$DC"
+
+  # Remove TMPDIR
+  cd "$TMPDIR/.." || { echo "something went horribly wrong" >&2; exit 1; };
+  echo /bin/rm -rf "$TMPDIR"
+
+  clean_house;
+
+  trap - INT TERM EXIT
+else
+  echo "Failed to acquire lockfile: $lockfile."
+  echo -n "Held by "
+  cat "$lockfile"
+  echo ""
+fi
 
 #EOF
